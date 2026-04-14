c7890854bb3833c0cabe1fa8e575b86d813dc2f1
fauzywan <iwanfauzay46@gmail.com>
2026-04-14 10:00:11 +0700

    pasti Pass

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    host: true,
     globals: true,
     environment: 'jsdom', // Ini kunci biar "document is not defined" hilang
   },
